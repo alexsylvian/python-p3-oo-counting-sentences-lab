7b0e4cef7fe24d6cff3edab573f234e6e780f658
alexsylvian <lazeratus1@gmail.com>
2024-01-17 22:37:32 -0500

    value setter

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+    def __init__(self, value=''):
+        self._value = ''  # Private attribute with leading underscore
+        self.value = value  # Use the setter method to validate and set the value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, new_value):
+        if not isinstance(new_value, str):
+            print("The value must be a string.")
+        self._value = new_value
+        
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+    
+    def count_sentences(self):
+        translator = str.maketrans({'.': '|', '?': '|', '!': '|'})
+        modified_value = self.value.translate(translator)
+        
+        sentences = [sentence for sentence in modified_value.split('|') if sentence.strip()]
+        
+        return len(sentences)
